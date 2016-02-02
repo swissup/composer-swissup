5b1863b92aec63ef10b1f621c6123b8435ef8723
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2016-02-02 22:39:52 +0200

    Inital commit

---
+++ src/Installer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Swissup\Composer;
+
+use Composer\Installer\LibraryInstaller;
+use Composer\Package\PackageInterface;
+
+class Installer extends LibraryInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        list($vendor, $name) = explode('/', $package->getPrettyName());
+
+        $vendor = ucfirst($vendor);
+        $name = str_replace('-', '', ucwords($name, '-'));
+
+        return getcwd() . '/release/' . $vendor . '/' . $name;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'magento2-module' === $packageType;
+    }
+}
